e7bc338cd83f639cd185efceb27c1f9e1d910071
Thayna <thayrcristina@gmail.com>
2023-12-12 23:10:52 -0300

    Little improvement in board printing

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -12,6 +12,10 @@ public class ChessPiece extends Piece {
         this.color = color;
     }
 
+    public Color getColor() {
+        return color;
+    }
+
     public void setColor(Color color) {
         this.color = color;
     }
